eb2465b9fb65d2232d5bb9cfb5d84df102af1910
Joaquín <joaquin.bj.97@gmail.com>
2018-02-20 21:21:53 +0100

    Adaptado a PDO

--- nuevo_usuario.php
+++ nuevo_usuario.php
@@ -1,9 +1,7 @@
 <?php
   //Requires
-  require 'elementos/bd.php';
+  require_once 'oad/funciones_oad.php';
 
-  //Conectar a la base de datos
-  $conexionbd = new yemi_bd();
 
   $msg2Class = "";
   $msg2 = "";
@@ -13,22 +11,24 @@
     if (isset($_GET['contrasenia'])) {
 
     $usuario = $_GET['usuario'];
-    $contras = $_GET['contrasenia'];
+    $contrasenia = $_GET['contrasenia'];
     $recontras = $_GET['recontrasenia'];
-    $correo = $_GET['correo'];
+    $tipo = $_GET['tipo'];
 
-      if ($contras == $recontras) {
-        //Comprobar si el usuario ya existe
-        $sql  = "SELECT * FROM usuarios WHERE nombre='$usuario';";
-        $reg = $conexionbd->sentencia($sql); //Aplicar la sentencia
+      if ($contrasenia == $recontras) {
+        //Comprobar si el usuario existe
+        $sql = datos_select("*","usuarios","?");
+        $reg = datos_ejecutar($sql,$usuario);
+
         // Si el usuario ya existe
         if ($reg->rowCount()) {
           $msg2Class = "rojo";
           $msg2 = "¡Este usuario ya ha sido registrado!";
         } else {
-          //Registrar nuevo usuario si éste aún no existe.
-          $sql  = "INSERT INTO usuarios VALUES (NULL, '$usuario', '$contras', '1')";
-          $reg = $conexionbd->sentencia($sql);
+          //Registrar nuevo usuario si este aún no existe.
+          $sql2 = datos_insert("usuarios (`nombre`, `contrasenia`, `tipo`)","(?, ?, ?)");
+          $reg2 = datos_ejecutar($sql2,$usuario,$contrasenia,$tipo);
+
           $msg2Class = "mensaje";
           $msg2 = "Usuario registrado con éxito";
         }
@@ -46,8 +46,8 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, user-scalable=no">
-    <link rel="stylesheet" href="../estilo/w3.css">
-    <link rel="stylesheet" href="../estilo/color_flower.css">
+    <link rel="stylesheet" href="css/w3.css">
+    <link rel="stylesheet" href="css/color_flower.css">
     <title>Crear usuario</title>
   </head>
   <body class="color-flower5">
@@ -56,8 +56,8 @@
       <div class="w3-third  w3-container"></div>
       <!-- Formulario  -->
       <div class="w3-third  w3-container">
-        <div>
-          <img src="../img/yemi.png">
+        <div class="w3-center">
+          <img src="img/tarea.png">
         </div>
 
         <div class=" w3_shadow">
@@ -70,7 +70,8 @@
               <input class="w3-input w3-padding-16" type="text" name="usuario" placeholder="NOMBRE DE USUARIO" required>
               <input class="w3-input w3-padding-16" type="password" name="contrasenia" maxlength="8" placeholder="CONTRASEÑA" required>
               <input class="w3-input w3-padding-16" type="password" name="recontrasenia" placeholder="REPETIR CONTRASEÑA" required>
-              <input class="w3-input w3-padding-16" type="text" name="correo" placeholder="CORREO">
+              <input class="w3-input w3-padding-16" type="number" min="0" max="1" name="tipo">
+                <label for="tipo">(0 - Admin / 1 - Normal)</label>
               <div class="w3-container w3-center w3-row w3-padding-16">
               <h3><input class="w3-btn color-flower2 w3-half" type="submit" value="REGISTRAR">
                 <a href="index.php"><div class="w3-btn color-flower3 w3-half "> Volver</div></a>
